1419100faee8bbcda16a60c01823495720493efa
livviee <84366762+livviee@users.noreply.github.com>
2023-11-30 06:19:35 +0900

    Add files via upload

--- entropy_sample_selection_in_processing_ver.py
+++ entropy_sample_selection_in_processing_ver.py
@@ -29,7 +29,6 @@ class ASR(sb.core.Brain):
     
         batch = batch.to(self.device)
         wavs, wav_lens = batch.sig
-        tokens_bos, _ = batch.tokens_bos
         wavs, wav_lens = wavs.to(self.device), wav_lens.to(self.device)
         
         if stage == sb.Stage.TRAIN:
@@ -49,7 +48,6 @@ class ASR(sb.core.Brain):
         p_ctc, wav_lens, feats = predictions
         
         ids = batch.id
-        tokens_eos, tokens_eos_lens = batch.tokens_eos
         tokens, tokens_lens = batch.tokens
         tokens, tokens_lens = tokens.to(self.device), tokens_lens.to(self.device)
 
@@ -95,7 +93,8 @@ class ASR(sb.core.Brain):
             self.cer_metric.append(ids, predicted_words, target_words)
 
         return p_ctc, feats, losses, loss
-
+    
+    
     def fit_batch(self, batch):
         """Train the parameters given a single batch in input"""
         
@@ -141,32 +140,43 @@ class ASR(sb.core.Brain):
 
             p_ctc, feats, losses, loss = self.compute_objectives(outputs, batch, sb.Stage.TRAIN)
 
+
+            if self.sample_selection:
+                # Entropy-based sample selection
+                with torch.no_grad():
+                    if not self.reservoir.init_phase and self.n_diff is None:
+                        self.n_diff = self.reservoir.count_k_i[self.reservoir.majority_group] - self.reservoir.count_k_i[self.reservoir.second_majority_group]
+
+                        if (self.n_diff == 0):
+                            min_dist_ids, min_dist_objects = find_min_dist_sample_in_majority_group(self.reservoir, 1, self)
+                        else:
+                            min_dist_ids, min_dist_objects = find_min_dist_sample_in_majority_group(self.reservoir, self.n_diff, self)
+                        
+                        if self.attribute == "age":
+                            min_dist_group = min_dist_objects[0].age
+                        elif self.attribute == "gender":
+                            min_dist_group = min_dist_objects[0].gender
+                            
+                        self.reservoir.delete_samples(min_dist_group, min_dist_ids)
+                    
+                    times, appended_num_list = append_batch_to_group_dict2(batch, feats, p_ctc, losses, self) # self.attribute, reservoir, asr
+                    
+                    if not self.reservoir.init_phase: 
+                        if times == self.n_diff:
+                            self.n_diff = None
+                        elif self.n_diff is not None:               
+                            self.n_diff -= times
+                    
+                weight = torch.ones(len(batch.id)).to(self.device)
+                for i in range(len(batch.id)):
+                    if i in appended_num_list:
+                        weight[i] = (1+self.lambda_star)
+                losses = losses * weight
+                loss = torch.mean(losses)
+                
+                        
             wandb.log({"Training loss": loss})
             
-            # Entropy-based sample selection
-            if not self.reservoir.init_phase and self.n_diff is None:
-                self.n_diff = self.reservoir.count_k_i[self.reservoir.majority_group] - self.reservoir.count_k_i[self.reservoir.second_majority_group]
-
-                if (self.n_diff == 0):
-                    min_dist_ids, min_dist_objects = find_min_dist_sample_in_majority_group(self.reservoir, 1, self)
-                else:
-                    min_dist_ids, min_dist_objects = find_min_dist_sample_in_majority_group(self.reservoir, self.n_diff, self)
-                
-                if self.attribute == "age":
-                    min_dist_group = min_dist_objects[0].age
-                elif self.attribute == "gender":
-                    min_dist_group = min_dist_objects[0].gender
-                    
-                self.reservoir.delete_samples(min_dist_group, min_dist_ids)
-            
-            times = append_batch_to_group_dict2(batch, feats, p_ctc, losses, self) # self.attribute, reservoir, asr
-            
-            if not self.reservoir.init_phase: #and (self.n_diff is not None):
-                if times == self.n_diff:
-                    self.n_diff = None
-                elif self.n_diff is not None: # times < n_diff                    
-                    self.n_diff -= times
-        
             
             with self.no_sync(not should_step):
                 (loss / self.grad_accumulation_factor).backward()
@@ -193,6 +203,7 @@ class ASR(sb.core.Brain):
         self.on_fit_batch_end(batch, outputs, loss, should_step)
         return loss.detach().cpu()
 
+            
     def on_fit_batch_end(self, batch, outputs, loss, should_step):
         """Called after ``fit_batch()``, meant for calculating and logging metrics.
 
@@ -234,6 +245,11 @@ class ASR(sb.core.Brain):
 
     def on_stage_start(self, stage, epoch):
         """Gets called at the beginning of each epoch"""
+        if epoch >= self.hparams.sample_selection_epoch:
+            self.sample_selection = True
+        else:
+            self.sample_selection = False
+        
         if stage != sb.Stage.TRAIN:
             self.cer_metric = self.hparams.cer_computer()
             self.wer_metric = self.hparams.error_rate_computer()
@@ -251,6 +267,8 @@ class ASR(sb.core.Brain):
             
             csv_file_ = dir_name + "/" + without_ext + "_EPOCH_" + str(epoch) + "." + ext
             create_csv(csv_file_, self.reservoir)
+            
+            self.reservoir.reinit()
             
         else:
             stage_stats["CER"] = self.cer_metric.summarize("error_rate")
@@ -291,9 +309,12 @@ class ASR(sb.core.Brain):
         )
         if self.checkpointer is not None:
             self.checkpointer.add_recoverable("modelopt", self.model_optimizer)
+            #self.checkpointer.add_recoverable("reservoir", self.reservoir)
             
     def zero_grad(self, set_to_none=False):
         self.model_optimizer.zero_grad(set_to_none)
+    
+    
     
 
 
@@ -302,7 +323,10 @@ class ASR(sb.core.Brain):
 if __name__ == "__main__":
 
     wandb.init(project='In-process_Entropy_sample_selection')
+    
+    #wandb.init(id="usual-armadillo-30", resume=True)
     wandb.run.save()
+    
         
     # Load hyperparameters file with command-line overrides
     hparams_file, run_opts, overrides = sb.parse_arguments(sys.argv[1:])
@@ -381,12 +405,14 @@ if __name__ == "__main__":
     # Create the datasets objects as well as tokenization and encoding
     train_data, valid_data, test_data = dataio_prepare(hparams, tokenizer)
 
+    checkpointer = hparams["checkpointer"]
+    
     # Trainer initialization
     asr_brain = ASR(
         modules=hparams["modules"],
         hparams=hparams,
         run_opts=run_opts,
-        checkpointer=hparams["checkpointer"],
+        checkpointer=checkpointer, #hparams["checkpointer"],
     )
 
     # Adding objects to trainer.
@@ -397,9 +423,12 @@ if __name__ == "__main__":
     
     asr_brain.reservoir = Reservoir(asr_brain.hparams.reservoir_size, asr_brain.hparams.attribute)
     asr_brain.n_diff = None
+   
     
     asr_brain.csv_file = asr_brain.hparams.selected_sample_csv
     asr_brain.attribute = asr_brain.hparams.attribute
+    asr_brain.lambda_star = asr_brain.hparams.lambda_star
+    asr_brain.sample_selection = False
     
     
     asr_brain.fit(
